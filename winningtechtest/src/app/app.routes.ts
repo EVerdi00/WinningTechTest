f15e0852d36241bb77cc5a7b443c3173c79024f5
EVerdi00 <erfan.verdi@gmail.com>
2026-07-14 00:38:36 +1000

    Create router paths

--- winningtechtest/src/app/app.routes.ts
+++ winningtechtest/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { ProductListing } from './features/product-listing/product-listing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'products', component: ProductListing },
+  { path: '**', redirectTo: '' }
+];
