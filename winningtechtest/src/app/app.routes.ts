42598741f51541c83b30f5965f077e2acf64e2e8
EVerdi00 <erfan.verdi@gmail.com>
2026-07-15 01:39:21 +1000

    add route titles per page

--- winningtechtest/src/app/app.routes.ts
+++ winningtechtest/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Home } from './features/home/home';
 import { ProductListing } from './features/product-listing/product-listing';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'products', component: ProductListing },
+  { path: '', component: Home, title: 'WinningStore' },
+  { path: 'products', component: ProductListing, title: 'Products | WinningStore' },
   { path: '**', redirectTo: '' }
 ];
